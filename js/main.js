9a626a14afe20ce42aeaaf319d0a419d0aadf3e9
Firdavs Gafurjonov <firdavs.gafurjonov@gmail.com>
2021-02-09 01:26:54 +0500

    toggle menu added

--- js/main.js
+++ js/main.js
@@ -1,14 +1,31 @@
+var nav = document.getElementById("nav");
+var navLinks = document.getElementById("nav-links");
+var navLines = document.getElementById("nav-line");
+var btn = document.getElementById("menu-btn");
+var xBtn = document.getElementById("menu-exit-btn");
+
 function toggleMenu() {
-    var x = document.getElementById("nav-links");
-    var y = document.getElementById("nav-line");
-    var nav = document.getElementById("nav");
-    if (x.style.display === "none") 
-    {
-      x.style.display = "block";
-      nav.style.height="100vh";
+    if (navLinks.style.display === "none") {
+        navLinks.style.display = "block";
+        nav.style.height="100vh";
+        btn.style.display = "none";
+        xBtn.style.display = "block";
+        navLines.style.display = "none";
     } 
     else {
-      x.style.display = "none";
-      nav.style.height= "10vh";
+        navLinks.style.display = "none";
+        nav.style.height= "10vh";
+        xBtn.style.display = "none";
+        btn.style.display = "block";
     }
-  }
+}
+function exitMenu() {
+    if(btn.style.display === "none"){
+        btn.style.display = "block";
+        xBtn.style.display = "none";
+    }
+    else {
+        btn.style.display = "none";
+        xBtn.style.display = "block";
+    }
+}
