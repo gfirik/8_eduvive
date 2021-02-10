226d9ae132d930f674081ca36f475f24526ed972
Firdavs Gafurjonov <firdavs.gafurjonov@gmail.com>
2021-02-10 13:58:49 +0500

    responsived

--- js/main.js
+++ js/main.js
@@ -1,31 +1,33 @@
-var nav = document.getElementById("nav");
-var navLinks = document.getElementById("nav-links");
-var navLines = document.getElementById("nav-line");
-var btn = document.getElementById("menu-btn");
-var xBtn = document.getElementById("menu-exit-btn");
+// var nav = document.getElementById("nav");
+// var navLinks = document.getElementById("nav-links");
+// var navLines = document.getElementById("nav-line");
+// var btn = document.getElementById("menu-btn");
+// var xBtn = document.getElementById("menu-exit-btn");
 
-function toggleMenu() {
-    if (navLinks.style.display === "none") {
-        navLinks.style.display = "block";
-        nav.style.height="100vh";
-        btn.style.display = "none";
-        xBtn.style.display = "block";
-        navLines.style.display = "none";
-    } 
-    else {
-        navLinks.style.display = "none";
-        nav.style.height= "10vh";
-        xBtn.style.display = "none";
-        btn.style.display = "block";
-    }
-}
-function exitMenu() {
-    if(btn.style.display === "none"){
-        btn.style.display = "block";
-        xBtn.style.display = "none";
-    }
-    else {
-        btn.style.display = "none";
-        xBtn.style.display = "block";
-    }
-}
+// function toggleMenu() {
+//     if (navLinks.style.display === "none") {
+//         navLinks.style.display = "block";
+//         btn.style.display = "none";
+//         navLines.style.display = "none";
+//         nav.style.height="100vh";
+//         xBtn.style.display = "block";
+//     } 
+//     else {
+//         navLinks.style.display = "none";
+//         nav.style.height= "10vh";
+//         xBtn.style.display = "none";
+//         btn.style.display = "block";
+//     }
+//     return(0);
+// }
+// function exitMenu() {
+//     if(btn.style.display === "none"){
+//         btn.style.display = "block";
+//         xBtn.style.display = "none";
+//     }
+//     else {
+//         btn.style.display = "none";
+//         xBtn.style.display = "block";
+//     }
+//     return(0);
+// }
