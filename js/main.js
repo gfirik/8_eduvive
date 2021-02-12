e6b7c829a6ee383fe8d820538486cb1e078dcfd6
Firdavs Gafurjonov <firdavs.gafurjonov@gmail.com>
2021-02-13 00:13:13 +0500

    menu btn completely added(lack of transition)

--- js/main.js
+++ js/main.js
@@ -1,15 +1,30 @@
+var mainNav = document.getElementById("nav");
 var nav = document.getElementById("nav-links");
-var btns = nav.getElementsByClassName("nav-a-list");
-for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
+var links = nav.getElementsByClassName("nav-a-list");
+var menuBtn = document.getElementById("menu-btn");
+var exitBtn = document.getElementById("menu-exit-btn");
+var aLink = nav.getElementsByClassName("active");
+
+for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function() {
     var current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
+    function myFunction(windowSize) {
+        if(windowSize.matches){
+            nav.style.display="none";
+            exitBtn.style.display="none";
+            menuBtn.style.display="flex";
+        } 
+        else {}
+    }
+    var windowSize = window.matchMedia("(max-width: 666px");
+    myFunction(windowSize);
+    windowSize.addEventListener(myFunction);
     });
 }
 
-var menuBtn = document.getElementById("menu-btn");
-var exitBtn = document.getElementById("menu-exit-btn");
+
 function toggleMenu() {
     if(nav.style.display="none"){
         nav.style.display="flex";
@@ -21,7 +36,6 @@ function toggleMenu() {
         exitBtn.style.display="none";
     }
 }
-
 function exitMenu() {
     if(nav.style.display="flex") {
         nav.style.display="none";
@@ -34,6 +48,15 @@ function exitMenu() {
         menuBtn.style.display="none";
     }
 }
-// nav.style.display="none";
-//     exitBtn.style.display="none";
-//     menuBtn.style.display="flex";
+
+function myFunction(windowSize) {
+    if(windowSize.matches){
+        nav.style.display="none";
+        exitBtn.style.display="none";
+        menuBtn.style.display="flex";
+    } 
+    else {}
+}
+var windowSize = window.matchMedia("(max-width: 666px");
+myFunction(windowSize);
+windowSize.addEventListener(myFunction);
